0923cb869ad0b26d6edc4efb2fe7cb46537b58db
Ulimit-Wynn <40670075+Ulimit-Wynn@users.noreply.github.com>
2018-07-26 14:21:02 +0100

    Successfuly implemented the optimize.minimise function, need to calculate p in a more accurate function

--- Testing file.py
+++ Testing file.py
@@ -1,15 +1,82 @@
 import scipy.integrate as sp
-import numpy as np
+import scipy.optimize as optimize
 from solver import DifferentiableFunction
+import solver
+
+x0 = 10
 
 
-def f_eval(t):
-    return np.array([2 * t, t-1])
+def u_eval(t):
+    return 0
 
-f = DifferentiableFunction(f_eval)
-f.to_vector()
 
-print(f.vector)
-print(f.evaluate(5))
-f.vector = 2 * f.vector
-print(f.evaluate(5))
+def dudx(u):
+    def func(t):
+        return (u.evaluate(t) - u.evaluate(t + 0.00001)) / 0.00001
+
+    return func
+
+
+def dudu(u):
+    def func(t):
+        return 1
+
+    return func
+
+
+u = DifferentiableFunction(u_eval, dfdx=dudx)
+u.to_vector()
+
+
+def x(u, t):
+    return sp.quad(u.evaluate, 0, t)[0] + x0
+
+
+def g_eval(u):
+    def func(t):
+        return x(u, t) ** 2 + u.evaluate(t) ** 2
+
+    return func
+
+
+def dgdx(u):
+    def func(t):
+        return 2 * x(u, t)
+
+    return func
+
+
+def dgdu(u):
+    def func(t):
+        return 2 * u.evaluate(t)
+
+    return func
+
+
+def h_eval(t):
+    return 0
+
+
+def J(vector):
+    u = DifferentiableFunction(vector=vector, dim=1)
+    u.to_func()
+    g = DifferentiableFunction(f=g_eval(u))
+    return sp.quad(g.evaluate, 0, g.T)[0]
+
+
+h = DifferentiableFunction(h_eval, dfdx=h_eval, dfdu=h_eval)
+
+
+def grad_wrapper(vector):
+    f = DifferentiableFunction(vector=vector, dim=1)
+    f.to_func()
+    f.dx = dudx(f)
+    f.du = dudu(f)
+    print(type(f.evaluate(10)))
+    g = DifferentiableFunction(f=g_eval(f), dfdx=dgdx(f), dfdu=dgdu(f))
+    h = DifferentiableFunction(f=h_eval, dfdx=h_eval, dfdu=h_eval)
+    return solver.gradient(f, g, h)
+
+
+result = optimize.minimize(J, u.vector, jac=grad_wrapper)
+print(result)
--- solver.py
+++ solver.py
@@ -1,5 +1,4 @@
 import scipy.integrate as integrate
-import scipy.optimize as optimize
 import numpy as np
 
 
@@ -7,29 +6,35 @@ class DifferentiableFunction:
     T = 360
     dt = 0.1
 
-    def __init__(self, f, vector=None, dim=None, dfdx=None, dfdu=None):
+    def __init__(self, f=None, vector=None, dim=None, dfdx=None, dfdu=None):
         self.evaluate = f
         self.dx = dfdx
         self.du = dfdu
+        self.vector = vector
+        self.dim = dim
 
     def to_vector(self):
         n = np.size(self.evaluate(0))
-        v = np.array([self.evaluate(i * self.dt) for i in range(0, int(self.T / self.dt)+1)])
-        print(v)
+        v = np.array([self.evaluate(i * self.dt) for i in range(0, int(self.T / self.dt) + 1)])
         v = np.ravel(v)
         self.vector = v
         self.dim = n
+
     def to_func(self):
         v = np.reshape(self.vector, (-1, self.dim))
+
         def func(t):
             return v[int(t / self.dt)]
+
+        self.evaluate = func
 
 
 def gradient(f, g, h):
     def func(t, y):
-        return f.dx(t) @ y + g.dx(f.T)
+        return np.atleast_1d(np.atleast_1d(f.dx(t)) @ np.atleast_1d(y) + g.dx(t))
 
-    p_values = integrate.solve_ivp(func, (0, f.T), h.dx(f.T), t_eval=[i for i in range(0, f.T)]).y
+    print('calculating p')
+    p_values = integrate.solve_ivp(func, (0, g.T), np.atleast_1d(h.dx(f.T)), t_eval=[i for i in range(0, g.T + 1)]).y
     for i in range(0, np.size(p_values, 0)):
         p_values[i] = p_values[i][::-1]
 
@@ -38,14 +43,8 @@ def gradient(f, g, h):
         return np.array([i[j] for i in p_values])
 
     def grad_eval(t):
-        return f.dx(t) @ p(t)
+        return np.atleast_1d(np.atleast_1d(f.du(t)) @ p(t) + g.du(t))
 
     grad = DifferentiableFunction(grad_eval, dfdx=None, dfdu=None)
     grad.to_vector()
     return grad.vector
-
-
-def J(u, g, h):
-    return integrate.quad(g.evaluate, 0, DifferentiableFunction.T, args=(u)) + h(DifferentiableFunction.T, u)
-
-print(optimize.minimize(J, u0, args=()))
