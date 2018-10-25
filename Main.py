05fe28fe1bca54bb187a8857007dc53cf95e23a8
Ulimit-Wynn <40670075+Ulimit-Wynn@users.noreply.github.com>
2018-10-25 16:51:21 +0100

    Testing branches

--- Main.py
+++ Main.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 import sympy
 import time as chrono
 
-alpha = G * M
-x0 = np.array([6371000, 0, 0, 0, 505846])
+alpha = Grav * M
+x0 = np.array([10, 0, 0, 0, 1])
 t = sympy.symbols('t')
 x, y, vx, vy, m = sympy.symbols('x y vx vy m')
 thrust, theta = sympy.symbols('thrust theta')
 X = sympy.Matrix([x, y, vx, vy, m])
 U = sympy.Matrix([thrust, theta])
 r = sympy.sqrt(x ** 2 + y ** 2)
+v = sympy.sqrt((vx ** 2 + vy ** 2))
 r1 = (x * vx + y * vy) / r
 phi = sympy.atan2(y, x)
 phi1 = (vx * y - x * vy) / (x ** 2 + y ** 2)
@@ -22,8 +23,9 @@ for i in range(0, n):
     thrust_matrix[i][2 * i] = 1
 for i in range(0, n):
     fuel_matrix[2 * i] = dt / isp
-ax = thrust * sympy.cos(theta) / m - alpha * x / (r ** 3) + vx * thrust / (isp * g0 * m)
-ay = thrust * sympy.sin(theta) / m - alpha * y / (r ** 3) + vy * thrust / (isp * g0 * m)
+drag = 0.5 * p0 * sympy.exp(-(r - 1) / h0) * Cd * A * v
+ax = thrust * sympy.cos(theta) / m - alpha * x / (r ** 3) + 0 * vx * thrust / (isp * g0 * m) - drag * vx
+ay = thrust * sympy.sin(theta) / m - alpha * y / (r ** 3) + 0 * vy * thrust / (isp * g0 * m) - drag * vy
 mdot = -thrust / isp / g0
 
 energy = (1 / 2 * (vx ** 2 + vy ** 2) - alpha / r)
@@ -32,30 +34,56 @@ moment = (vx * y - x * vy)
 F = sympy.Matrix([vx, vy, ax, ay, mdot])
 
 H = (moment - moment0) ** 2 / (moment0 ** 2) + (energy - energy0) ** 2 / (energy0 ** 2)
-G = 0 * (thrust / (isp * g0)) / 505000
+H_moment = (moment - moment0) ** 2 / (moment0 ** 2)
+H_energy = (energy - energy0) ** 2 / (energy0 ** 2)
+H_zero = 0 * x
+G_zero = 0 * thrust
+G = 0 * (thrust / (isp * g0)) + 0.01 / (r - 9.5)
 dFdU = F.jacobian(U)
 dFdX = F.jacobian(X)
 dHdX = H.diff(X)
+dHdX_zero = H_zero.diff(X)
+dHdX_moment = H_moment.diff(X)
+dHdX_energy = H_energy.diff(X)
+dGdU_zero = G_zero.diff(U)
+dGdX_zero = G_zero.diff(X)
 dGdU = G.diff(U)
 dGdX = G.diff(X)
 f_tem = sympy.lambdify((t, U, X), F)
 dfdx = sympy.lambdify((t, U, X), dFdX)
 dfdu_tem = sympy.lambdify((t, U, X), dFdU)
 h_eval = sympy.lambdify((X,), H)
+h_zero_eval = sympy.lambdify((X,), H_moment)
+h_moment_eval = sympy.lambdify((X,), H_moment)
+h_energy_eval = sympy.lambdify((X,), H_energy)
 dhdx_tem = sympy.lambdify((X,), dHdX)
+dhdx_zero_tem = sympy.lambdify((X,), dHdX_zero)
+dhdx_moment_tem = sympy.lambdify((X,), dHdX_moment)
+dhdx_energy_tem = sympy.lambdify((X,), dHdX_energy)
+g_zero_eval = sympy.lambdify((U, X), G_zero)
 g_eval = sympy.lambdify((U, X), G)
+dgdx_zero_tem = sympy.lambdify((U, X), dGdX_zero)
+dgdu_zero_tem = sympy.lambdify((U, X), dGdU_zero)
 dgdx_tem = sympy.lambdify((U, X), dGdX)
 dgdu_tem = sympy.lambdify((U, X), dGdU)
 
 
-def f_eval(time, u_at_t, x_at_t):
-    return f_tem(time, u_at_t, x_at_t).ravel()
+def f_eval(time_value, u_at_t, x_at_t):
+    return f_tem(time_value, u_at_t, x_at_t).ravel()
 
 
-def dfdu(time, u_at_t, x_at_t):
+def dfdu(time_value, u_at_t, x_at_t):
     if u_at_t[0] == 0:
         return np.array([[0, 0], [0, 0], [1 / x_at_t[4], 0], [0, 1 / x_at_t[4]], [0, 0]])
-    return dfdu_tem(time, u_at_t, x_at_t)
+    return dfdu_tem(time_value, u_at_t, x_at_t)
+
+
+def dgdu_zero(u_at_t, x_at_t):
+    return dgdu_zero_tem(u_at_t, x_at_t).ravel()
+
+
+def dgdx_zero(u_at_t, x_at_t):
+    return dgdx_zero_tem(u_at_t, x_at_t).ravel()
 
 
 def dgdu(u_at_t, x_at_t):
@@ -70,68 +98,127 @@ def dhdx(x_at_t):
     return dhdx_tem(x_at_t).ravel()
 
 
-def u_eval(time):
-    if time<200:
-        return np.array([5100000, time/200])
-    return np.array([801000, np.pi/2])
+def dhdx_moment(x_at_t):
+    return dhdx_moment_tem(x_at_t).ravel()
+
+
+def dhdx_energy(x_at_t):
+    return dhdx_energy_tem(x_at_t).ravel()
+
+
+def dhdx_zero(x_at_t):
+    return dhdx_zero_tem(x_at_t).ravel()
+
+
+def u_eval(time_value):
+    if time_value < 250 / 100:
+        return np.array([6585000 * newton_to_force_unit_coeff, 0])
+    elif time_value < 500 / 100:
+        return np.array([4000000 * newton_to_force_unit_coeff, np.pi / 2])
+    return np.array([0 * newton_to_force_unit_coeff, np.pi / 2])
+
 
 def thrust_constraint(vector):
     res = thrust_matrix @ vector
     return res
 
 
-def fuel_constraint(vector):
-    return 31200 - np.sum(vector[::2]) * dt / (isp * g0)
+def first_fuel_constraint(vector):
+    return 381412 * kg_to_mass_unit__coeff * kg_to_mass_unit__coeff - np.sum(vector[:int(T_first/dt):2]) * dt / (isp * g0)
+
+beep
+def second_fuel_constraint(vector):
+    return 2
+
+
+def solve_for_orbit(x_at_t0):
+    def func(time_value, y):
+        dy = np.array([y[2], y[3], - alpha * y[0] / (np.sqrt(y[0] ** 2 + y[1] ** 2) ** 3),
+                       - alpha * y[1] / (np.sqrt(y[0] ** 2 + y[1] ** 2) ** 3), 0])
+        return dy
+
+    solve = integrate.solve_ivp(func, (0, 600), x_at_t0, dense_output=True, rtol=10 ** (-13), atol=10 ** (-8)).sol
+    solution = TimeFunction(f=solve.__call__)
+    return solution
+
 
 f = DifferentiableFunction(f=f_eval, dfdx=dfdx, dfdu=dfdu)
+g_zero = DifferentiableFunction(f=g_zero_eval, dfdx=dgdx_zero, dfdu=dgdu_zero)
 g = DifferentiableFunction(f=g_eval, dfdx=dgdx, dfdu=dgdu)
 h = DifferentiableFunction(f=h_eval, dfdx=dhdx)
+h_zero = DifferentiableFunction(f=h_zero_eval, dfdx=dhdx_zero)
+h_moment = DifferentiableFunction(f=h_moment_eval, dfdx=dhdx_moment)
+h_energy = DifferentiableFunction(f=h_energy_eval, dfdx=dhdx_energy)
 system = DynamicalSystem(f, x0)
 J = Functional(system, g, h)
+J_zero = Functional(system, g, h_zero)
+J_moment = Functional(system, g_zero, h_moment)
+J_energy = Functional(system, g_zero, h_energy)
 u = TimeFunction(u_eval)
 u.to_vector()
 
-en = sympy.lambdify((X,), energy)
-mom = sympy.lambdify((X,), moment)
-id_en = en(np.array([6.29155739e+06, 1.70173759e+06, -3.03191864e+03, 9.65228389e+03,
-  5.04009971e+04]))
-print(energy0)
-id_mom = mom(np.array([6.29155739e+06, 1.70173759e+06, -3.03191864e+03, 9.65228389e+03,
-  5.04009971e+04]))
-print(moment0)
-id_e = np.sqrt(1 + 2 * id_en * (id_mom ** 2)/(alpha ** 2))
-print(id_e)
-print(id_mom ** 2 / alpha /(1 - id_e ** 2))
 start = chrono.time()
-result = optimize.minimize(J.J_wrapper, u.vector, jac=J.grad_wrapper, method='COBYLA', constraints=({"type": "ineq", "fun": thrust_constraint}, {"type": "ineq", "fun": fuel_constraint}))
-time_array = np.linspace(0, T, n)
+# result = optimize.minimize(J.J_wrapper, u.vector, tol=1e-9, jac=J.grad_wrapper,
+#                           constraints=({"type": "ineq", "fun": thrust_constraint},
+#                                        {"type": "ineq", "fun": fuel_constraint},
+#                                        {"type": "eq", "fun": J_moment.J_wrapper, "jac": J_moment.grad_wrapper},
+#                                        {"type": "eq", "fun": J_energy.J_wrapper, "jac": J_energy.grad_wrapper}))
+result = optimize.minimize(J.J_wrapper, u.vector, tol=1e-9, jac=J.grad_wrapper,
+                           constraints=({"type": "ineq", "fun": thrust_constraint},
+                                        {"type": "ineq", "fun": fuel_constraint}))
 print(result)
 u1 = TimeFunction(vector=result.x, dim=2)
 u1.to_func()
-"""grad = result.jac
-print("final grad: ", grad)
+grad = result.jac
 d = np.ones(np.size(u.vector))
-eps = 0.0001
-u_test1 = TimeFunction(vector=u1.vector + eps * d, dim=u.dim)
-u_test2 = TimeFunction(vector=u1.vector - eps * d, dim=u.dim)
-print("d @ grad: ", d @ grad)
-print("Finite difference: ", (J(u_test1) + J(u_test2)) / 2 / eps)
-"""
+for i in range(2, 10):
+    print(" ")
+    print("eps=", "10^-", str(i))
+    eps = 10 ** (-1 * i)
+    u_test1 = TimeFunction(vector=u1.vector + eps * d, dim=u.dim)
+    u_test2 = TimeFunction(vector=u1.vector - eps * d, dim=u.dim)
+    print("d @ grad: ", d @ grad)
+    print("Finite difference: ", (J(u_test1) - J(u_test2)) / 2 / eps)
+time_array_u = np.linspace(0, T, n)
+time_array_x = np.linspace(0, T, 20000)
 P = TimeFunction(f=system.solve(u1))
-P.to_vector()
+P.to_vector(step=20000)
+orbit = solve_for_orbit(P(T))
+orbit.to_vector(step=20000, period=600)
+orbit_x = orbit.vector[::5]
+orbit_y = orbit.vector[1::5]
+earth_x = 10 * np.array([np.cos(i * 2 * np.pi / 20000) for i in range(0, 20000)])
+earth_y = 10 * np.array([np.sin(i * 2 * np.pi / 20000) for i in range(0, 20000)])
+ideal_orbit_x = a0 * np.array([np.cos(i * 2 * np.pi / 20000) for i in range(0, 20000)])
+ideal_orbit_y = a0 * np.array([np.sin(i * 2 * np.pi / 20000) for i in range(0, 20000)])
+print(P(T))
 X1 = P.vector[::5]
 X2 = P.vector[1::5]
-print(P(T))
+Mass = P.vector[4::5]
+gr1 = grad[::2]
+gr2 = grad[1::2]
 plt.figure(1)
-plt.plot(time_array, X1)
+plt.subplot(211)
+plt.plot(time_array_x, X1)
+plt.subplot(212)
+plt.plot(time_array_x, X2)
 plt.figure(2)
-plt.plot(time_array, X2)
-plt.figure(3)
+plt.ylim((15, -15))
+plt.xlim((15, -15))
+plt.autoscale(False)
+plt.plot(earth_x, earth_y)
 plt.plot(X1, X2)
+plt.figure(3)
+plt.subplot(211)
+plt.plot(time_array_u, u1.vector[::2])
+plt.plot(time_array_u, u.vector[::2])
+plt.subplot(212)
+plt.plot(time_array_u, u1.vector[1::2])
+plt.plot(time_array_u, u.vector[1::2])
 plt.figure(4)
-plt.plot(time_array, u1.vector[::2])
-plt.figure(5)
-plt.plot(time_array, u1.vector[1::2])
+plt.plot(earth_x, earth_y)
+plt.plot(orbit_x, orbit_y)
+plt.plot(ideal_orbit_x, ideal_orbit_y)
 end = chrono.time()
 print("time: ", end - start)
 plt.show()
