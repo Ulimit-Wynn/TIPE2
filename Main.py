ec1bf249819182360d2e4d830f03b15b2ead92b6
Ulimit-Wynn <40670075+Ulimit-Wynn@users.noreply.github.com>
2018-10-31 20:51:51 +0000

    Working simulation to LEO with isp=300

--- Main.py
+++ Main.py
@@ -33,7 +33,7 @@ moment = (vx * y - x * vy)
 
 F = sympy.Matrix([vx, vy, ax, ay, mdot])
 H = (v - v_ideal) ** 2 / (v_ideal ** 2) + (r - a0) ** 2 / (a0 ** 2) + (vx * x + vy * y) ** 2
-G = 0 * (thrust / (isp * g0)) + 0.001 / (r - 9.2)
+G = 0 * (thrust / (isp * g0))
 dFdU = F.jacobian(U)
 dFdX = F.jacobian(X)
 dHdX = H.diff(X)
@@ -75,13 +75,16 @@ def u_eval(time_value):
     if time_value < 70 * time_coff:
         return np.array([9.806 * 21920 * newton_to_force_unit_coeff, 0])
     else:
-        return np.array([9.806 * 11920 * newton_to_force_unit_coeff, (time_value - 70 * time_coff) * np.pi/T])
+        return np.array([9.806 * (21920 - time_value * 21920 / T) * newton_to_force_unit_coeff, (time_value - 70 * time_coff) * np.pi/T])
 
 
-def thrust_constraint(vector):
+def thrust_constraint_min(vector):
     res = thrust_matrix @ vector
     return res
 
+def thrust_constraint_max(vector):
+    res = 33500 * 9.806 * newton_to_force_unit_coeff * np.ones(np.size(vector)//2) - thrust_matrix @ vector
+    return res
 
 def fuel_constraint(vector):
     return (16290 - 550) * kg_to_mass_unit__coeff - np.sum(vector[::2]) * dt / isp / g0
@@ -94,7 +97,7 @@ def solve_for_orbit(x_at_t0):
                        - alpha * y[1] / (np.sqrt(y[0] ** 2 + y[1] ** 2) ** 3), 0])
         return dy
 
-    solve = integrate.solve_ivp(func, (0, 600), x_at_t0, dense_output=True, rtol=10 ** (-13), atol=10 ** (-8)).sol
+    solve = integrate.solve_ivp(func, (0, 600), x_at_t0, dense_output=True, rtol=1e-13, atol=1-8).sol
     solution = TimeFunction(f=solve.__call__)
     return solution
 
@@ -109,40 +112,34 @@ u.to_vector()
 
 start = chrono.time()
 
-result = optimize.minimize(J.J_wrapper, u.vector, tol=1e-4, jac=J.grad_wrapper,
-                           constraints=({"type": "ineq", "fun": thrust_constraint},
+result = optimize.minimize(J.J_wrapper, u.vector, method="SLSQP", options={"ftol": 1e-12}, jac=J.grad_wrapper,
+                           constraints=({"type": "ineq", "fun": thrust_constraint_min},
+                                        {"type": "ineq", "fun": thrust_constraint_max},
                                         {"type": "ineq", "fun": fuel_constraint}))
 print(result)
 u1 = TimeFunction(vector=result.x, dim=2)
 u1.to_func()
 grad = result.jac
 d = np.ones(np.size(u.vector))
-for i in range(2, 10):
-    print(" ")
-    print("eps=", "10^-", str(i))
-    eps = 10 ** (-1 * i)
-    u_test1 = TimeFunction(vector=u1.vector + eps * d, dim=u.dim)
-    u_test2 = TimeFunction(vector=u1.vector - eps * d, dim=u.dim)
-    print("d @ grad: ", d @ grad)
-    print("Finite difference: ", (J(u_test1) - J(u_test2)) / 2 / eps)
+
 time_array_u = np.linspace(0, T, n)
-time_array_x = np.linspace(0, T, 20000)
+time_array_x = np.linspace(0, T, 5000)
 P_original = system.solve(u)
 P = TimeFunction(f=system.solve(u1))
-P_original.to_vector(step=20000)
-P.to_vector(step=20000)
+P_original.to_vector(step=5000)
+P.to_vector(step=5000)
 orbit_original = solve_for_orbit(P_original(T))
 orbit = solve_for_orbit(P(T))
-orbit_original.to_vector(step=20000, period=600)
-orbit.to_vector(step=20000, period=600)
+orbit_original.to_vector(step=5000, period=600)
+orbit.to_vector(step=5000, period=600)
 orbit_x = orbit.vector[::5]
 orbit_y = orbit.vector[1::5]
 orbit_original_x = orbit_original.vector[::5]
 orbit_original_y = orbit_original.vector[1::5]
-earth_x = 10 * np.array([np.cos(i * 2 * np.pi / 20000) for i in range(0, 20000)])
-earth_y = 10 * np.array([np.sin(i * 2 * np.pi / 20000) for i in range(0, 20000)])
-ideal_orbit_x = a0 * np.array([np.cos(i * 2 * np.pi / 20000) for i in range(0, 20000)])
-ideal_orbit_y = a0 * np.array([np.sin(i * 2 * np.pi / 20000) for i in range(0, 20000)])
+earth_x = 10 * np.array([np.cos(i * 2 * np.pi / 5000) for i in range(0, 5000)])
+earth_y = 10 * np.array([np.sin(i * 2 * np.pi / 5000) for i in range(0, 5000)])
+ideal_orbit_x = a0 * np.array([np.cos(i * 2 * np.pi / 5000) for i in range(0, 5000)])
+ideal_orbit_y = a0 * np.array([np.sin(i * 2 * np.pi / 5000) for i in range(0, 5000)])
 print(P(T))
 X1 = P.vector[::5]
 X2 = P.vector[1::5]
@@ -170,7 +167,6 @@ plt.plot(time_array_u, u.vector[1::2])
 plt.figure(4)
 plt.plot(earth_x, earth_y)
 plt.plot(orbit_x, orbit_y)
-plt.plot(orbit_original_x, orbit_original_y)
 plt.plot(ideal_orbit_x, ideal_orbit_y)
 end = chrono.time()
 print("time: ", end - start)
